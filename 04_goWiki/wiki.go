8f3a22cfaefc43537b209fd5330f33efc0f55dab
cher1 <chue.her@autotrader.com>
2017-04-30 15:18:30 -0400

    b/c i'm happy

---
+++ 04_goWiki/wiki.go
@@ -0,0 +1,64 @@
+package main
+
+// Editing Pages
+// The html/template package
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+// Page struct
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	// This function will work fine, but all that hard-coded HTML is ugly.
+	// Of course, there is a better way.
+	/*fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+	"<form action=\"/save/%s\" method=\"POST\">"+
+	"<textarea name=\"body\">%s</textarea><br>"+
+	"<input type=\"submit\" value=\"Save\">"+
+	"</form>",
+	p.Title, p.Title, p.Body)*/
+
+	// Use templates to abstract the html code
+	t, _ := template.ParseFiles("edit.gohtml")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+}
