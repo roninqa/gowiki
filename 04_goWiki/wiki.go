63b812d988e635a4df630250ba1367517b275dfc
cher1 <chue.her@autotrader.com>
2017-05-01 21:17:24 -0400

    b/c i'm happy

--- 04_goWiki/wiki.go
+++ 04_goWiki/wiki.go
@@ -59,6 +59,6 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
